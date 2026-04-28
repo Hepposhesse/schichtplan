e67e782f0565b2b3f66e65f701f4ce7b038ffc2e
Hepposhesse <shesse@trustedservices.info>
2026-04-28 11:47:44 +0200

    Delete Terminplaner AA24 directory

--- Terminplaner AA24/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://fxvicaazpcyfzkxtriau.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4dmljYWF6cGN5ZnpreHRyaWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNjQ5NjcsImV4cCI6MjA5Mjg0MDk2N30.yIMSd-z1FFmWSEdLlsF_G4-PA8PHGESH0gQhod03BTg'
-)
